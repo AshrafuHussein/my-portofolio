658339c0797030f4d7e79eeac60c3d5a0ea57d0f
AshrafuHussein <ashrafuhussien@gmail.com>
2026-08-28 13:42:28 +0300

    chore: initialize project build artifacts in .next directory

---
+++ .next/server/vendor-chunks/@designcodeio.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/@designcodeio";
+exports.ids = ["vendor-chunks/@designcodeio"];
+exports.modules = {
+
+/***/ "(rsc)/./node_modules/@designcodeio/threeui/lib-dist/style.css":
+/*!***************************************************************!*\
+  !*** ./node_modules/@designcodeio/threeui/lib-dist/style.css ***!
+  \***************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"41033081c22b\");\nif (false) {}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9ub2RlX21vZHVsZXMvQGRlc2lnbmNvZGVpby90aHJlZXVpL2xpYi1kaXN0L3N0eWxlLmNzcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsaUVBQWUsY0FBYztBQUM3QixJQUFJLEtBQVUsRUFBRSxFQUF1QiIsInNvdXJjZXMiOlsid2VicGFjazovL2FzaC1wb3J0Zm9saW8vLi9ub2RlX21vZHVsZXMvQGRlc2lnbmNvZGVpby90aHJlZXVpL2xpYi1kaXN0L3N0eWxlLmNzcz80ZGYyIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IFwiNDEwMzMwODFjMjJiXCJcbmlmIChtb2R1bGUuaG90KSB7IG1vZHVsZS5ob3QuYWNjZXB0KCkgfVxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(rsc)/./node_modules/@designcodeio/threeui/lib-dist/style.css\n");
+
+/***/ })
+
+};
+;
